6d1f3149d849bdccab1905f006a51c351c6aa390
Toluwanimi Agboola <toluwanimiagboola@gmail.com>
2021-08-10 14:34:41 +0100

    Initial commit

---
+++ src/components/Button.vue
@@ -0,0 +1,20 @@
+<template>
+	<button class="btn" @click="onClick()" :style="{ background: color }">
+		{{ text }}
+	</button>
+</template>
+
+<script>
+export default {
+	name: "Button",
+	props: {
+		text: String,
+		color: String,
+	},
+	methods: {
+		onClick() {
+			this.$emit("btn-click");
+		},
+	},
+};
+</script>
